2db15f5a2838cd346819fd188bd56454b84279ba
JohanLindkvist <johanlindkvist91@gmail.com>
2017-04-04 12:04:58 +0200

    Added param input for y and z, posistion reference and apply button

--- CF_GUI.py
+++ CF_GUI.py
@@ -29,21 +29,64 @@ paramFrame.pack(side=LEFT)
 plotFrame = Frame(root, width = 1000,height = 250, relief=SUNKEN)
 plotFrame.pack(side=BOTTOM)
 
+#String variables for input
 PDx_K = StringVar()
 PDx_Td = StringVar()
+PDy_K = StringVar()
+PDy_Td = StringVar()
+PDz_K = StringVar()
+PDz_Td = StringVar()
+pos_ref = StringVar()
+
 # Input for K parameter for PD controller of x coordinate 
-param_PDx_K_Lbl = Label(paramFrame, font=('arial', 10,'bold'), text = "PDx K", bd = 16,anchor = 'w')
+param_PDx_K_Lbl = Label(paramFrame, font=('arial', 10,'bold'), text = "PDx K", bd = 10,anchor = 'w')
 param_PDx_K_Lbl.grid(row=1,column=0)
-txtTest = Entry(paramFrame, font=('arial', 10,'bold'), textvariable = PDx_K, bd=10,insertwidth=2, bg="powder blue", justify = 'right')
-txtTest.grid(row=1,column=1)
+PDx_K_Entry = Entry(paramFrame, font=('arial', 10,'bold'), textvariable = PDx_K, bd=10,insertwidth=2, bg="powder blue", justify = 'right')
+PDx_K_Entry.grid(row=1,column=1)
 
 # Input for Td parameter for PD controller of x coordinate 
-param_PDx_Td_Lbl = Label(paramFrame, font=('arial', 10,'bold'), text = "PDx Td", bd = 16,anchor = 'w')
+param_PDx_Td_Lbl = Label(paramFrame, font=('arial', 10,'bold'), text = "PDx Td", bd = 10,anchor = 'w')
 param_PDx_Td_Lbl.grid(row=2,column=0)
-txtTest = Entry(paramFrame, font=('arial', 10,'bold'), textvariable = PDx_Td, bd=10,insertwidth=2, bg="powder blue", justify = 'right')
-txtTest.grid(row=2,column=1)
+PDx_Td_Entry = Entry(paramFrame, font=('arial', 10,'bold'), textvariable = PDx_Td, bd=10,insertwidth=2, bg="powder blue", justify = 'right')
+PDx_Td_Entry.grid(row=2,column=1)
+
+# Input for K parameter for PD controller of y coordinate 
+param_PDy_K_Lbl = Label(paramFrame, font=('arial', 10,'bold'), text = "PDy K", bd = 10,anchor = 'w')
+param_PDy_K_Lbl.grid(row=3,column=0)
+PDy_K_Entry = Entry(paramFrame, font=('arial', 10,'bold'), textvariable = PDy_K, bd=10,insertwidth=2, bg="powder blue", justify = 'right')
+PDy_K_Entry.grid(row=3,column=1)
+
+# Input for Td parameter for PD controller of y coordinate 
+param_PDy_Td_Lbl = Label(paramFrame, font=('arial', 10,'bold'), text = "PDy Td", bd = 10,anchor = 'w')
+param_PDy_Td_Lbl.grid(row=4,column=0)
+PDy_Td_Entry = Entry(paramFrame, font=('arial', 10,'bold'), textvariable = PDy_Td, bd=10,insertwidth=2, bg="powder blue", justify = 'right')
+PDy_Td_Entry.grid(row=4,column=1)
+
+# Input for K parameter for PD controller of z coordinate 
+param_PDz_K_Lbl = Label(paramFrame, font=('arial', 10,'bold'), text = "PDz K", bd = 10,anchor = 'w')
+param_PDz_K_Lbl.grid(row=5,column=0)
+PDz_K_Entry = Entry(paramFrame, font=('arial', 10,'bold'), textvariable = PDz_K, bd=10,insertwidth=2, bg="powder blue", justify = 'right')
+PDz_K_Entry.grid(row=5,column=1)
+
+# Input for Td parameter for PD controller of z coordinate 
+param_PDz_Td_Lbl = Label(paramFrame, font=('arial', 10,'bold'), text = "PDz Td", bd = 10,anchor = 'w')
+param_PDz_Td_Lbl.grid(row=6,column=0)
+PDz_Td_Entry = Entry(paramFrame, font=('arial', 10,'bold'), textvariable = PDz_Td, bd=10,insertwidth=2, bg="powder blue", justify = 'right')
+PDz_Td_Entry.grid(row=6,column=1)
+
+# Input for reference position
+pos_ref_Lbl = Label(paramFrame, font=('arial', 10,'bold'), text = "Pos Ref", bd = 10,anchor = 'w')
+pos_ref_Lbl.grid(row=0,column=0)
+pos_ref_Entry = Entry(paramFrame, font=('arial', 10,'bold'), textvariable = pos_ref, bd=10,insertwidth=2, bg="powder blue", justify = 'right')
+pos_ref_Entry.grid(row=0,column=1)
+
+#Define method for apply button
+def btnApply():
+    pos_ref.set("x,y,z")
+# Apply changes from input above
+Apply = Button(paramFrame, padx=8,pady=8,bd=6,fg="black", font=('arial', 10,'bold'),text = "Apply", bg="powder blue",command =btnApply).grid(row=7, column = 1)
 
 
 
-
+ 
 root.mainloop()
