0bab424592387ca01a2bf8ee49f4802cafa3279b
JohanLindkvist <johanlindkvist91@gmail.com>
2017-04-13 14:24:55 +0200

    Redesigned the GUI

--- CF_GUI.py
+++ CF_GUI.py
@@ -20,26 +20,23 @@ class GUI:
 
     def __init__(self,root):    
         self.root=root
+        self.messageBox = tkinter.messagebox
         #Size of window
         root.geometry("1000x600+0+0")
         #Window title
         root.title("CrazyFlie control system")
-        
-        #Top frame
-        self.Tops = Frame(root, width = 1000,height = 40, bg ="powder blue", relief=SUNKEN)
-        self.Tops.pack(side=TOP)
-        
-        #Label in top frame
-        self.lblName = Label(self.Tops, font =('arial', 30,'bold'), text = "CrazyFlie control system", fg = "Steel blue", bd = 10, anchor = 'w')
-        self.lblName.grid(row=0,column=0) 
-        
+                
         #Upper left frame for parameters and reference values
-        self.paramFrame = Frame(root, width = 500,height = 360, relief=SUNKEN)
-        self.paramFrame.pack(side=LEFT)
+        self.paramFrame = Frame(root, width = 530,height = 340, relief=SUNKEN)
+        self.paramFrame.place(x = 460, y = 10, width = 530, height = 250)
         
         #Lower left frame for x, y and z-plots
-        self.plotFrame = Frame(root, width = 1000,height = 200, relief=SUNKEN)
-        self.plotFrame.pack(side=BOTTOM)
+        self.XplotFrame = Frame(root, width = 460, height = 200, relief=SUNKEN)
+        self.XplotFrame.place(x = 0, y = 0, width = 460, height = 190)
+        self.YplotFrame = Frame(root, width = 460, height = 200, relief=SUNKEN)
+        self.YplotFrame.place(x = 0, y = 200, width = 460, height = 190)
+        self.ZplotFrame = Frame(root, width = 460, height = 200, relief=SUNKEN)
+        self.ZplotFrame.place(x = 0, y = 400, width = 460, height = 190)
         
         #String variables for input
         self.PDx_K = StringVar()
@@ -66,70 +63,107 @@ class GUI:
         self.Tdz = 1
         self.PDz_Td.set(self.Tdz)
         
-        self.pos_ref = StringVar()
+        self.x_ref = StringVar()
+        self.y_ref = StringVar()
+        self.z_ref = StringVar()
         self.ref = [1.00, 2.00, 3.14]
-        self.pos_ref.set(self.ref)
+        self.x_ref.set(self.ref[0])
+        self.y_ref.set(self.ref[1])
+        self.z_ref.set(self.ref[2])
         
-        # Input for K parameter for PD controller of x coordinate 
-        self.param_PDx_K_Lbl = Label(self.paramFrame, font=('arial', 10,'bold'), text = "PDx K", bd = 10,anchor = 'w')
-        self.param_PDx_K_Lbl.grid(row=2,column=0,columnspan=2)
-        self.PDx_K_Entry = Entry(self.paramFrame, font=('arial', 10,'bold'), textvariable = self.PDx_K, bd=10,insertwidth=2, bg="powder blue", justify = 'right')
-        self.PDx_K_Entry.grid(row=2,column=2,columnspan=3)
+        #Input fields for pos ref
+        self.ref_lbl = Label(self.paramFrame, font=('arial', 10, 'bold'), text = "Reference position", bd = 10, anchor = 'w')
+        self.ref_lbl.grid(row = 0, column = 0, columnspan = 4)
+        self.ref_x_lbl = Label(self.paramFrame, font=('arial', 10, 'bold'), text = "x", bd = 10, anchor = 'w')
+        self.ref_x_lbl.grid(row = 1, column = 0)
+        self.ref_x_entry = Entry(self.paramFrame, font=('arial', 10, 'bold'), textvariable = self.x_ref, bd = 10, bg = "powder blue", justify = 'right', width = 5)
+        self.ref_x_entry.grid(row = 1, column = 1)
+        self.ref_y_lbl = Label(self.paramFrame, font = ('arial', 10, 'bold'), text = "y", bd = 10, anchor = 'w')
+        self.ref_y_lbl.grid(row = 2, column = 0)
+        self.ref_y_entry = Entry(self.paramFrame, font=('arial', 10, 'bold'), textvariable = self.y_ref, bd = 10, bg = "powder blue", justify = 'right', width = 5)
+        self.ref_y_entry.grid(row = 2, column = 1)
+        self.ref_z_lbl = Label(self.paramFrame, font = ('arial', 10, 'bold'), text = "z", bd = 10, anchor = 'w')
+        self.ref_z_lbl.grid(row = 3, column = 0)
+        self.ref_z_entry = Entry(self.paramFrame, font=('arial', 10, 'bold'), textvariable = self.z_ref, bd = 10, bg = "powder blue", justify = 'right', width = 5)
+        self.ref_z_entry.grid(row = 3, column = 1)
         
-        # Input for Td parameter for PD controller of x coordinate 
-        self.param_PDx_Td_Lbl = Label(self.paramFrame, font=('arial', 10,'bold'), text = "PDx Td", bd = 10,anchor = 'w')
-        self.param_PDx_Td_Lbl.grid(row=3,column=0,columnspan=2)
-        self.PDx_Td_Entry = Entry(self.paramFrame, font=('arial', 10,'bold'), textvariable = self.PDx_Td, bd=10,insertwidth=2, bg="powder blue", justify = 'right')
-        self.PDx_Td_Entry.grid(row=3,column=2,columnspan=3)
         
-        # Input for K parameter for PD controller of y coordinate 
-        self.param_PDy_K_Lbl = Label(self.paramFrame, font=('arial', 10,'bold'), text = "PDy K", bd = 10,anchor = 'w')
-        self.param_PDy_K_Lbl.grid(row=4,column=0,columnspan=2)
-        self.PDy_K_Entry = Entry(self.paramFrame, font=('arial', 10,'bold'), textvariable = self.PDy_K, bd=10,insertwidth=2, bg="powder blue", justify = 'right')
-        self.PDy_K_Entry.grid(row=4,column=2,columnspan=3)
-        
-        # Input for Td parameter for PD controller of y coordinate 
-        self.param_PDy_Td_Lbl = Label(self.paramFrame, font=('arial', 10,'bold'), text = "PDy Td", bd = 10,anchor = 'w')
-        self.param_PDy_Td_Lbl.grid(row=5,column=0,columnspan=2)
-        self.PDy_Td_Entry = Entry(self.paramFrame, font=('arial', 10,'bold'), textvariable = self.PDy_Td, bd=10,insertwidth=2, bg="powder blue", justify = 'right')
-        self.PDy_Td_Entry.grid(row=5,column=2,columnspan=3)
-        
-        # Input for K parameter for PD controller of z coordinate 
-        self.param_PDz_K_Lbl = Label(self.paramFrame, font=('arial', 10,'bold'), text = "PDz K", bd = 10,anchor = 'w')
-        self.param_PDz_K_Lbl.grid(row=6,column=0,columnspan=2)
-        self.PDz_K_Entry = Entry(self.paramFrame, font=('arial', 10,'bold'), textvariable = self.PDz_K, bd=10,insertwidth=2, bg="powder blue", justify = 'right')
-        self.PDz_K_Entry.grid(row=6,column=2,columnspan=3)
-        
-        # Input for Td parameter for PD controller of z coordinate 
-        self.param_PDz_Td_Lbl = Label(self.paramFrame, font=('arial', 10,'bold'), text = "PDz Td", bd = 10,anchor = 'w')
-        self.param_PDz_Td_Lbl.grid(row=7,column=0,columnspan=2)
-        self.PDz_Td_Entry = Entry(self.paramFrame, font=('arial', 10,'bold'), textvariable = self.PDz_Td, bd=10,insertwidth=2, bg="powder blue", justify = 'right')
-        self.PDz_Td_Entry.grid(row=7,column=2,columnspan=3)
-        
-        # Input for reference position
-        self.pos_ref_Lbl = Label(self.paramFrame, font=('arial', 10,'bold'), text = "Pos Ref", bd = 10,anchor = 'w')
-        self.pos_ref_Lbl.grid(row=1,column=0,columnspan=2)
-        self.pos_ref_Entry = Entry(self.paramFrame, font=('arial', 10,'bold'), textvariable = self.pos_ref, bd=10,insertwidth=2, bg="powder blue", justify = 'right',  width=20)
-        self.pos_ref_Entry.grid(row=1,column=2,columnspan=3)
-        
-       
-        
-        # Add plot to window 
-        self.f1 = Figure(figsize=(6,2), dpi=100)
+        #Input fields for PD parameters
+        #X-axis
+        self.param_lbl = Label(self.paramFrame, font=('arial', 10, 'bold'), text = "Controll parameters", bd = 10, anchor = 'w')
+        self.param_lbl.grid(row = 0, column = 4, columnspan = 5)
+        self.x_lbl = Label(self.paramFrame, font=('arial', 10, 'bold'), text = "x-axis:", bd = 10, anchor = 'w')
+        self.x_lbl.grid(row = 1, column = 4)
+        self.x_k_lbl = Label(self.paramFrame, font=('arial', 10, 'bold'), text = "K", bd = 10, anchor = 'w')
+        self.x_k_lbl.grid(row = 1, column = 5)
+        self.x_k_entry = Entry(self.paramFrame, font=('arial', 10, 'bold'), textvariable = self.PDx_K, bd = 10, bg = "powder blue", justify = 'right', width = 5)
+        self.x_k_entry.grid(row = 1, column = 6)
+        self.x_Td_lbl = Label(self.paramFrame, font=('arial', 10, 'bold'), text = "Td", bd = 10, anchor = 'w')
+        self.x_Td_lbl.grid(row = 1, column = 7)
+        self.x_Td_entry = Entry(self.paramFrame, font=('arial', 10, 'bold'), textvariable = self.PDx_Td, bd = 10, bg = "powder blue", justify = 'right', width = 5)
+        self.x_Td_entry.grid(row = 1, column = 8)
+        #y-axis
+        self.y_lbl = Label(self.paramFrame, font=('arial', 10, 'bold'), text = "y-axis:", bd = 10, anchor = 'w')
+        self.y_lbl.grid(row = 2, column = 4)
+        self.y_k_lbl = Label(self.paramFrame, font=('arial', 10, 'bold'), text = "K", bd = 10, anchor = 'w')
+        self.y_k_lbl.grid(row = 2, column = 5)
+        self.y_k_entry = Entry(self.paramFrame, font=('arial', 10, 'bold'), textvariable = self.PDy_K, bd = 10, bg = "powder blue", justify = 'right', width = 5)
+        self.y_k_entry.grid(row = 2, column = 6)
+        self.y_Td_lbl = Label(self.paramFrame, font=('arial', 10, 'bold'), text = "Td", bd = 10, anchor = 'w')
+        self.y_Td_lbl.grid(row = 2, column = 7)
+        self.y_Td_entry = Entry(self.paramFrame, font=('arial', 10, 'bold'), textvariable = self.PDx_Td, bd = 10, bg = "powder blue", justify = 'right', width = 5)
+        self.y_Td_entry.grid(row = 2, column = 8)
+        #z-axis
+        self.z_lbl = Label(self.paramFrame, font=('arial', 10, 'bold'), text = "z-axis:", bd = 10, anchor = 'w')
+        self.z_lbl.grid(row = 3, column = 4)
+        self.z_k_lbl = Label(self.paramFrame, font=('arial', 10, 'bold'), text = "K", bd = 10, anchor = 'w')
+        self.z_k_lbl.grid(row = 3, column = 5)
+        self.z_k_entry = Entry(self.paramFrame, font=('arial', 10, 'bold'), textvariable = self.PDz_K, bd = 10, bg = "powder blue", justify = 'right', width = 5)
+        self.z_k_entry.grid(row = 3, column = 6)
+        self.z_Td_lbl = Label(self.paramFrame, font=('arial', 10, 'bold'), text = "Td", bd = 10, anchor = 'w')
+        self.z_Td_lbl.grid(row = 3, column = 7)
+        self.z_Td_entry = Entry(self.paramFrame, font=('arial', 10, 'bold'), textvariable = self.PDz_Td, bd = 10, bg = "powder blue", justify = 'right', width = 5)
+        self.z_Td_entry.grid(row = 3, column = 8)
+                
+        #Add plot to window for X-axis 
+        self.f1 = Figure(figsize=(10,3), dpi=50)
         self.a1 = self.f1.add_subplot(111)
         self.a1.plot([1,2,3,4,5,6,7,8],[5,6,1,3,8,9,3,5])
-        self.canvas1 = FigureCanvasTkAgg(self.f1, self.plotFrame)
+        self.canvas1 = FigureCanvasTkAgg(self.f1, self.XplotFrame)
         self.canvas1.show()
-        self.canvas1.get_tk_widget().pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=True)
-        self.toolbar1 = NavigationToolbar2TkAgg(self.canvas1, self.plotFrame)
+        self.canvas1.get_tk_widget().pack(side=tkinter.TOP, expand=True)
+        self.toolbar1 = NavigationToolbar2TkAgg(self.canvas1, self.XplotFrame)
         self.toolbar1.update()
-        self.canvas1._tkcanvas.pack(side=tkinter.TOP, fill=tkinter.BOTH, expand=True)
+        self.canvas1._tkcanvas.pack(side=tkinter.TOP, expand=True)
         
-        self.Apply = Button(self.paramFrame, padx=6,pady=6,bd=6,fg="black", font=('arial', 10,'bold'),text = "Apply", bg="powder blue",command =self.btnApply).grid(row=0, column = 4)
-        self.GO = Button(self.paramFrame, padx=6,pady=6,bd=6,fg="black", font=('arial', 10,'bold'),text = "GO", bg="powder blue",command =self.btnGo).grid(row=0, column = 0)
-        self.Home = Button(self.paramFrame, padx=6,pady=6,bd=6,fg="black", font=('arial', 10,'bold'),text = "Home", bg="powder blue",command =self.btnHome).grid(row=0, column = 1)
-        self.Land = Button(self.paramFrame, padx=6,pady=6,bd=6,fg="black", font=('arial', 10,'bold'),text = "Land", bg="powder blue",command =self.btnLand).grid(row=0, column = 2)
-        self.Stop = Button(self.paramFrame, padx=6,pady=6,bd=6,fg="black", font=('arial', 10,'bold'),text = "Stop", bg="powder blue",command =self.btnStop).grid(row=0, column = 3)
+        #Add plot to window for Y-axis 
+        self.f2 = Figure(figsize=(10,3), dpi=50)
+        self.a2 = self.f2.add_subplot(111)
+        self.a2.plot([1,2,3,4,5,6,7,8],[5,6,1,3,8,9,3,5])
+        self.canvas2 = FigureCanvasTkAgg(self.f2, self.YplotFrame)
+        self.canvas2.show()
+        self.canvas2.get_tk_widget().pack(side=tkinter.TOP, expand=True)
+        self.toolbar2 = NavigationToolbar2TkAgg(self.canvas2, self.YplotFrame)
+        self.toolbar2.update()
+        self.canvas2._tkcanvas.pack(side=tkinter.TOP,fill=tkinter.BOTH, expand=True)
+        
+        #Add plot to window for Z-axis 
+        self.f3 = Figure(figsize=(10,3), dpi=50)
+        self.a3 = self.f3.add_subplot(111)
+        self.a3.plot([1,2,3,4,5,6,7,8],[5,6,1,3,8,9,3,5])
+        self.canvas3 = FigureCanvasTkAgg(self.f3, self.ZplotFrame)
+        self.canvas3.show()
+        self.canvas3.get_tk_widget().pack(side=tkinter.TOP, expand=True)
+        self.toolbar3 = NavigationToolbar2TkAgg(self.canvas3, self.ZplotFrame)
+        self.toolbar3.update()
+        self.canvas3._tkcanvas.pack(side=tkinter.TOP, expand=True)
+       
+        # Buttons
+        self.Apply = Button(self.paramFrame, padx=6,pady=6,bd=6,fg="black", font=('arial', 10,'bold'),text = "Apply", bg="powder blue",command =self.btnApply, width = 7).grid(row=4, column = 6)
+        self.GO = Button(self.paramFrame, padx=6,pady=6,bd=6,fg="black", font=('arial', 10,'bold'),text = "GO", bg="powder blue",command =self.btnGo, width = 7).grid(row=1, column = 2)
+        self.Home = Button(self.paramFrame, padx=6,pady=6,bd=6,fg="black", font=('arial', 10,'bold'),text = "Home", bg="powder blue",command =self.btnHome, width = 7).grid(row=1, column = 3)
+        self.Land = Button(self.paramFrame, padx=6,pady=6,bd=6,fg="black", font=('arial', 10,'bold'),text = "Land", bg="powder blue",command =self.btnLand, width = 7).grid(row=2, column = 2)
+        self.Stop = Button(self.paramFrame, padx=6,pady=6,bd=6,fg="black", font=('arial', 10,'bold'),text = "Stop", bg="powder blue",command =self.btnStop, width = 7).grid(row=2, column = 3)
         
         #Drop down menu File and Help    
         self.menu = Menu(root)
@@ -148,55 +182,58 @@ class GUI:
     
      #Define method for apply button (Not real method)
     def btnApply(self):
-        self.Kx = PDx_K.get()
-        self.PDx_K.set(Kx)
-        self.print ("PDx K = ", Kx)
-        self.Tdx = PDx_Td.get()
-        self.PDx_Td.set(Tdx)
-        print ("PDx Td = ", Tdx)
-        self.Ky = PDy_K.get()
-        self.PDy_K.set(Ky)
-        print ("PDy K = ", Ky)
-        self.Tdy = PDy_Td.get()
-        self.PDy_Td.set(Tdy)
-        print ("PDy Td = ", Tdy)
-        self.Kz = PDz_K.get()
-        self.PDz_K.set(Kz)
-        print ("PDz K = ", Kz)
-        self.Tdz = PDz_Td.get()
-        self.PDz_Td.set(Tdz) 
-        print ("PDz Td = ", Tdz)     
-        self.ref = pos_ref.get()
-        print ("Position reference is set to ", ref,)
-        self.pos_ref.set(ref)
-    
-    
-    # Apply changes from input above
-        
+        self.Kx = self.PDx_K.get()
+        self.PDx_K.set(self.Kx)
+        print ("PDx K = ", self.Kx)
+        self.Tdx = self.PDx_Td.get()
+        self.PDx_Td.set(self.Tdx)
+        print ("PDx Td = ", self.Tdx)
+        self.Ky = self.PDy_K.get()
+        self.PDy_K.set(self.Ky)
+        print ("PDy K = ", self.Ky)
+        self.Tdy = self.PDy_Td.get()
+        self.PDy_Td.set(self.Tdy)
+        print ("PDy Td = ", self.Tdy)
+        self.Kz = self.PDz_K.get()
+        self.PDz_K.set(self.Kz)
+        print ("PDz K = ", self.Kz)
+        self.Tdz = self.PDz_Td.get()
+        self.PDz_Td.set(self.Tdz) 
+        print ("PDz Td = ", self.Tdz)     
+                
     #Define method for GO! button
     def btnGo(self):
         # TODO implement method
         print ("GO")
-    #GO! button
-    
+        self.ref[0] = self.x_ref.get()
+        self.ref[1] = self.y_ref.get()
+        self.ref[2] = self.z_ref.get()
+        print ("Position reference is set to ", self.ref)
+        self.x_ref.set(self.ref[0])
+        self.y_ref.set(self.ref[1])
+        self.z_ref.set(self.ref[2])    
     
     #Defines method for Home button
     def btnHome(self):
         #TODO implement method
         print ("Home")
-    #Home button
+        self.ref[0] = 0
+        self.ref[1] = 0
+        self.ref[2] = 0
+        print ("Position reference is set to ", self.ref)
+        self.x_ref.set(self.ref[0])
+        self.y_ref.set(self.ref[1])
+        self.z_ref.set(self.ref[2])
     
     #Defines method for Land button
     def btnLand(self):
         #TODO implement method
         print ("Land")
-    #Land Button
     
     #Defines method for Stop button
     def btnStop(self):
         #TODO implement method
         print ("Stop")
-    #Stop button
     
     #Drop down menu
     def Connect(self):
@@ -206,7 +243,7 @@ class GUI:
         print ("Disconnect")
         
     def About(self):
-        self.tkinter.messagebox.showinfo("About", "CrazyFlie dude!")
+        self.messageBox.showinfo("About", "CrazyFlie dude!")
     
    
 
