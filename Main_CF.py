2b82aa8857689dee74dcd7f25bbd1634cdbfd609
Michael <git@michaelgabassi.com>
2017-04-13 14:07:36 +0200

    implemented main method threads

--- Main_CF.py
+++ Main_CF.py
@@ -1,7 +1,29 @@
 "main class to call other classes"
-from tkinter import *
+from Regul_CF import regulate
+import GUI_CF
+import multiprocessing
+
 
 def __init__(self):
     self.x = 'Main method'
 
     
+    
+def main():
+    #Create threads
+    regulator = multiprocessing.Process(target=regulate ,args=('Regulator',4,))
+    monitor = multiprocessing.Process(target=regulate, args=('Monitor',8,))
+    GUI = multiprocessing.Process(target=GUI_CF.main, args=('Monitor',8,))
+ 
+    #Start threads
+    regulator.start()
+    monitor.start()
+    GUI.start()
+         
+    
+    
+def terminateAll():
+    #terminate all of the processes
+    pass
+
+if __name__ == "__main__": main()
